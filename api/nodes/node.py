440f0bf92d251f3adc01e06545fc3045c90484c1
Kia <kiasn21@gmail.com>
2023-04-21 20:28:27 +0330

    update : docker

---
+++ api/nodes/node.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+name = os.getenv("NAME")
+port = int(os.getenv("PORT"))
+
+@app.route('/')
+def hello():
+    return f'This is {name}!'
+
+
+if __name__ == '__main__':
+    app.run(port=port)
