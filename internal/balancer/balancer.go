c0c4bfa4d452b6ae221acaf1f7ab89732a85e89f
Kia <kiasn21@gmail.com>
2023-04-28 21:17:46 +0330

    patch: checking all nodes before response

--- internal/balancer/balancer.go
+++ internal/balancer/balancer.go
@@ -1,31 +1,28 @@
 package balancer
 
 import (
+	"errors"
 	"math/rand"
 	"net/http"
 	"net/url"
 )
 
 type Balancer struct {
-	Nodes []*url.URL
-	Total int
+	Nodes   []*url.URL
+	Checked []*http.Response
+	Total   int
+	Count   int
 }
 
 // PickNode
 // This functions selects a node randomly
 // If one node is damaged, select another one
 func (b Balancer) PickNode(r *http.Request) (*http.Response, error) {
-	rand := rand.Intn(b.Total)
-	r.Host = b.Nodes[rand].Host
-	r.URL.Host = b.Nodes[rand].Host
-	r.URL.Scheme = b.Nodes[rand].Scheme
-	r.RequestURI = ""
-	response, err := http.DefaultClient.Do(r)
-	// counting error connections
-	count := 0
-	for err != nil || count <= 3 {
-		response, err = b.PickNode(r)
-		count++
+	b.CheckNodes(r)
+	rand := rand.Intn(len(b.Checked))
+	if len(b.Checked) == 0 {
+		return nil, errors.New("there is no node to response")
 	}
-	return response, err
+	// counting error connections
+	return b.Checked[rand], nil
 }
--- internal/balancer/judge.go
+++ internal/balancer/judge.go
@@ -1,20 +1,18 @@
 package balancer
 
 import (
-	"fmt"
 	"net/http"
 )
 
-type Nodes struct {
-	Response     *http.Response
-	ResponseTime float64
-}
-
-type Judge struct {
-	Nodes []Nodes
-}
-
-func (j Judge) ChooseURL() *http.Response {
-	fmt.Println(j)
-	return j.Nodes[1].Response
+func (b *Balancer) CheckNodes(r *http.Request) {
+	for _, node := range b.Nodes {
+		r.Host = node.Host
+		r.URL.Host = node.Host
+		r.URL.Scheme = node.Scheme
+		r.RequestURI = ""
+		response, err := http.DefaultClient.Do(r)
+		if err == nil {
+			b.Checked = append(b.Checked, response)
+		}
+	}
 }
